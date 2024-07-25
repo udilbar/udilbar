13f9317221b952ae324fdac3e457e4666b0b3b7e
udilbar <Bokhodir.urinboev@gmail.com>
2024-07-25 12:11:29 +0500

    [update] about section info

--- components/sections/about/cozy/about.tsx
+++ components/sections/about/cozy/about.tsx
@@ -8,36 +8,43 @@ import { ArrowUpRightIcon } from 'lucide-react';
 
 function About() {
   return (
-    <MotionWrap className="w-full py-24 lg:py-32" id="about">
+    <MotionWrap className="w-full pt-24 lg:pt-32" id="about">
       {/* TODO: Redesign for horizontal */}
-      <div className="space-y-4 px-4 md:px-6 lg:space-y-10">
-        <div className="flex w-full flex-col items-center justify-center text-center lg:flex-row lg:justify-between lg:text-left">
-          <div className="flex flex-col items-center lg:items-start">
+      <div className="items-center justify-between gap-12 space-y-4 px-4 md:px-6 lg:flex lg:space-y-10">
+        <div className="flex w-full flex-col text-center lg:text-left">
+          <div className="flex flex-col items-start">
             <Reveal>
-              <h2 className="text-4xl font-bold leading-tight tracking-tighter sm:text-5xl md:text-5xl md:leading-tight lg:text-6xl lg:leading-tight">
-                About
-              </h2>
-            </Reveal>
-            <Reveal>
-              <h2 className="-mt-2 text-4xl font-bold leading-tight tracking-tighter sm:text-5xl md:text-5xl md:leading-tight lg:text-6xl lg:leading-tight">
-                Me
+              <h2 className="text-4xl font-bold leading-tight tracking-tighter sm:text-5xl md:text-5xl md:leading-tight lg:mb-4 lg:text-6xl lg:leading-tight">
+                About Me
               </h2>
             </Reveal>
           </div>
-          <p className="mt-4 hidden text-gray-500 dark:text-gray-400 lg:mt-0 lg:block lg:w-[35%]">
+          <p className="mt-4 hidden max-w-[500px] text-gray-500 dark:text-gray-400 lg:mt-0 lg:block">
             Here&apos;s where I share my journey through tech, highlighting the
             experiences and passions that drive my innovative pursuits.
           </p>
         </div>
         <div className="space-y-4">
-          <p className="mt-6 max-w-[700px] text-gray-500 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-            I am a passionate and creative Web Developer with a love for
-            beautiful and functional websites. I have experience working with a
-            variety of web technologies and frameworks and I am always eager to
-            learn new things and take on new challenges.
+          <p className="max-w-[1200px] text-gray-500 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+            I am your go-to frontend web developer for small and medium-sized
+            businesses.
+            <br />
+            I am passionate about delivering results that endure:
+            <br />
+            - 🚀 Velocity
+            <br />
+            - 🔒 Veracity
+            <br />
+            - 💼 Value
+            <br />I specialize in crafting precise, adaptive, custom web
+            applications that empower clients to excel in their markets. Major
+            corporations have seen profit boosts with my assistance.
           </p>
           <Button asChild>
-            <Link href="resume.pdf" target="_blank">
+            <Link
+              href="https://docs.google.com/document/d/1kJ-gtaNaCdVBo_FzHJFN-Tx6oWeazq9zlw6Ey7dOVBI/edit?usp=sharing"
+              target="_blank"
+            >
               View Resume <ArrowUpRightIcon className="ml-2 size-5" />
             </Link>
           </Button>
