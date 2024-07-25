0b2b3c5a8da24dd7ee511e9378a0bdd0555f3915
udilbar <Bokhodir.urinboev@gmail.com>
2024-07-25 10:02:28 +0500

    Initial commit from Create Next App

---
+++ app/config.ts
@@ -0,0 +1,26 @@
+import { Metadata } from '@/types/metadata';
+
+const metadata: Metadata = {
+  author: {
+    name: 'John Doe',
+    username: 'johndoe',
+    label: 'Designer & Developer'
+  },
+  site: {
+    title: 'John Doe - Portfolio Website',
+    description: 'Portfolio of web & UI designer/developer John Doe.',
+    ogImage: '/images/og.png',
+    url: 'https://www.example.com',
+    keywords: [
+      'web design',
+      'UI design',
+      'web development',
+      'portfolio',
+      'John Doe'
+    ],
+    language: 'en',
+    charset: 'UTF-8'
+  }
+};
+
+export { metadata };
