b92e2a2015566ffb242fb1dc4c75d2b4dd4af8ba
Dilbar Urinboeva <ndilbar1618@gmail.com>
2024-10-28 18:19:51 +0500

    [add] job openings platform data

--- components/sections/projects/config.ts
+++ components/sections/projects/config.ts
@@ -1,26 +1,19 @@
 import { Project } from '@/types/project';
 
 const projects: Project[] = [
-  {
-    name: 'Personal Portfolio Website',
-    slug: 'portfolio',
-    description:
-      'A personal portfolio to showcase coding projects, resume, and skills in a beautifully designed format.',
-    thumbnail: '/images/projects/portfolio/cover.jpg'
-  },
+  // {
+  //   name: 'Personal Portfolio Website',
+  //   slug: 'portfolio',
+  //   description:
+  //     'A personal portfolio to showcase coding projects, resume, and skills in a beautifully designed format.',
+  //   thumbnail: '/images/projects/portfolio/cover.jpg'
+  // },
   {
     name: 'Dizipro',
     slug: 'dizipro',
     description:
       'An online platform for custom, high-quality 3D modeling, offering a range of accurate and affordable models crafted by professional designers.',
     thumbnail: '/images/projects/dizipro/cover.jpg'
-  },
-  {
-    name: 'LexmeAI',
-    slug: 'lexme_ai',
-    description:
-      'An AI-driven chat application designed to provide users with reliable legal insights based on the Laws of the Republic of Uzbekistan and the Uzbek Constitution.',
-    thumbnail: '/images/projects/lexme_ai/cover.jpg'
   },
   {
     name: 'Imperium POS Cashier Dashboard',
@@ -37,11 +30,18 @@ const projects: Project[] = [
     thumbnail: '/images/projects/telecomchat/cover.jpg'
   },
   {
-    name: 'Job Posting Platform',
-    slug: 'job_postings',
+    name: 'LexmeAI',
+    slug: 'lexme_ai',
     description:
-      'A website offering interactive coding tutorials and challenges to help users learn programming languages through practice.',
-    thumbnail: '/images/projects/coding_tutorials/cover.jpg'
+      'An AI-driven chat application designed to provide users with reliable legal insights based on the Laws of the Republic of Uzbekistan and the Uzbek Constitution.',
+    thumbnail: '/images/projects/lexme_ai/cover.jpg'
+  },
+  {
+    name: 'Job Openings Platform',
+    slug: 'jobs_platform',
+    description:
+      'The job openings platform is a full-stack SPA built with Node.js and SQLite for multi-language support.',
+    thumbnail: '/images/projects/jobs_platform/cover.jpg'
   }
 ];
 
